96c582e940172caa85515d7777596e04bb363953
MauriNestor <mauric845apaza@gmail.com>
2025-03-16 23:31:15 -0400

    use of stateholder

---
+++ app/src/main/java/org/architect_course/ui/main/MainState.kt
@@ -0,0 +1,39 @@
+package org.architect_course.ui.main
+
+import android.Manifest
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import org.architect_course.model.Movie
+import org.architect_course.ui.common.PermissionRequester
+
+fun Fragment.buildMainState(
+    scope: CoroutineScope = viewLifecycleOwner.lifecycleScope,
+    navController: NavController = findNavController(),
+    locationPermissionRequester: PermissionRequester = PermissionRequester(
+        this,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
+) = MainState(scope, navController, locationPermissionRequester)
+
+class MainState(
+    private val scope: CoroutineScope,
+    private val navController: NavController,
+    private val locationPermissionRequester: PermissionRequester
+) {
+    fun onMovieClicked(movie: Movie) {
+        val action = MainFragmentDirections.actionMainToDetail(movie)
+        navController.navigate(action)
+    }
+
+    fun requestLocationPermission(afterRequest: (Boolean) -> Unit) {
+        scope.launch {
+            val result = locationPermissionRequester.request()
+            afterRequest(result)
+        }
+    }
+
+}
