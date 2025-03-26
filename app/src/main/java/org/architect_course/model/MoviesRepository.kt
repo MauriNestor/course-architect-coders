ce95f21211fa533c4d5cfb5f0051e117104c3aac
MauriNestor <mauric845apaza@gmail.com>
2025-03-25 23:07:09 -0400

    delete dependency of region reposotory in remotedatasource

--- app/src/main/java/org/architect_course/model/MoviesRepository.kt
+++ app/src/main/java/org/architect_course/model/MoviesRepository.kt
@@ -1,9 +1,7 @@
 package org.architect_course.model
 
 
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.withContext
 import org.architect_course.App
 import org.architect_course.R
 import org.architect_course.model.database.Movie
@@ -11,18 +9,16 @@ import org.architect_course.model.datasource.MovieLocalDataSource
 import org.architect_course.model.datasource.MovieRemoteDataSource
 
 class MoviesRepository(application: App) {
+    private val regionRepository = RegionRepository(application)
     private val localDataSource = MovieLocalDataSource(application.db.movieDao())
-    private val remoteDataSource = MovieRemoteDataSource(
-        application.getString(R.string.api_key),
-        regionRepository = RegionRepository(application)
-    )
+    private val remoteDataSource = MovieRemoteDataSource(application.getString(R.string.api_key))
     val popularMovies = localDataSource.movies
 
     fun findById(id: Int): Flow<Movie> = localDataSource.findById(id)
 
     suspend fun requestPopularMovies() {
         if (localDataSource.isEmpty()) {
-            val movies = remoteDataSource.findPopularMovies()
+            val movies = remoteDataSource.findPopularMovies(regionRepository.findLastRegion())
             localDataSource.save(movies.results.toLocalModel())
         }
     }
--- app/src/main/java/org/architect_course/model/datasource/MovieRemoteDataSource.kt
+++ app/src/main/java/org/architect_course/model/datasource/MovieRemoteDataSource.kt
@@ -1,15 +1,10 @@
 package org.architect_course.model.datasource
 
-import org.architect_course.model.RegionRepository
 import org.architect_course.model.RemoteConnection
 
 
-class MovieRemoteDataSource(private val apiKey: String, private val regionRepository: RegionRepository) {
+class MovieRemoteDataSource(private val apiKey: String) {
 
-    suspend fun findPopularMovies() =
-        RemoteConnection.service
-            .listPopularMovies(
-                apiKey,
-                regionRepository.findLastRegion()
-            )
+    suspend fun findPopularMovies(region: String) =
+        RemoteConnection.service.listPopularMovies(apiKey, region)
 }
