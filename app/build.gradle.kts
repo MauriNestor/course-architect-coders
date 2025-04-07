219b95d4d2320fb703d11b5bc66731932b443855
MauriNestor <mauric845apaza@gmail.com>
2025-04-06 23:59:05 -0400

    add error model to use in all the layers

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,4 +87,6 @@ dependencies {
     implementation ("androidx.room:room-runtime:2.5.1")
     implementation ("androidx.room:room-ktx:2.5.1")
     kapt ("androidx.room:room-compiler:2.5.1")
+
+    implementation ("io.arrow-kt:arrow-core:1.1.5")
 }
